1c5bd358a331f1c4cfac0319a1bbce0c77008439
Dan <dannyo0311@gmail.com>
2022-10-11 15:14:54 +0800

    add process_upload_feedback.py
    basic function works in Windows

---
+++ process_upload_feedback.py
@@ -0,0 +1,66 @@
+#! /usr/bin/env python3
+
+import os
+import re
+import requests
+
+def get_files_in_dir(dir_path):
+    """return target files in a list"""
+    items_list = os.listdir(dir_path)
+    item =""
+    files_list = []
+    for item in items_list:
+        # check isfile txt
+        if os.path.isfile(os.path.join(dir_path, item)):
+            if re.search(r"\.txt", os.path.join(dir_path, item)) != None:
+                files_list.append(item)
+
+    # check  print(files_list)
+    return files_list
+
+def extact_content(file_path):
+    """ create a dictionary by keeping title, name, date, and feedback as keys """
+    content_dict = {}
+    with open(file_path, 'r') as f:
+        content_dict["title"] = f.readline().strip()
+        content_dict["name"] = f.readline().strip()
+        content_dict["date"] = f.readline().strip()
+        content_dict["feedback"] = f.read().strip()
+
+    return content_dict
+
+    
+def post_the_dict():
+    """ to the company's website through requests module """
+    # http://<corpweb-external-IP>/feedback
+    pass
+
+def main():
+    """ make sure an error message isn't returned """
+    dir = ""
+    while True:
+        dir = os.path.abspath(input("please enter the directory: "))
+        print(dir)
+        if os.path.isdir(dir):
+            print("directory valid")
+            break
+        else:
+            print("directory invalid")
+
+    files_list = get_files_in_dir(dir)
+
+    content_list = []
+
+    for file in files_list:
+        print(os.path.join(dir, file))
+        content_list.append(extact_content(os.path.join(dir, file)))
+
+    # check print(content_list)
+
+    response = requests.get('https://www.google.com')
+    print(response.ok)
+    print(response.status_code)
+    print(response.raise_for_status())
+
+if __name__ == "__main__":
+    main()
